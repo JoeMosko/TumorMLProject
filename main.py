ad0160342241d3cf9a482fda441bc17b14b68dfa
Joey Mosko <joeymosko@gmail.com>
2025-06-01 17:39:57 -0600

    Ready for deployment

---
+++ main.py
@@ -0,0 +1,264 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
+from tensorflow.keras.utils import load_img # type: ignore
+from tensorflow.keras.utils import img_to_array # type: ignore
+import streamlit as st
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import time
+import os
+import cv2 
+import shutil
+# https://www.kaggle.com/datasets/rm1000/brain-tumor-mri-scans/data 
+
+
+#Need to split the data into...
+#70% training
+#15% test
+#15% validation
+#Each folder having 4 subfolders for each type of brain scan
+
+
+
+def moveHealthyFilesToTraining():
+
+    #For healthy...
+    SourceDir = "./images/healthy/"
+    TrainingDir = "./TrainingData/healthy/"
+
+    #Error handling (making sure both folders exist)
+    if not os.path.exists(SourceDir):
+        print(SourceDir, "does not exist")
+        return 
+    if not os.path.exists(TrainingDir):
+        print(TrainingDir, "does not exist")
+        return 
+
+    Files = os.listdir(SourceDir)
+    #Calculate 75 percent of the files to be moved
+    numToMove = int(len(Files) * .75)
+    #Parse 75 percent of the files to a new array
+    selectedFiles = Files[:numToMove]
+
+    #loop through all files in selectedFiles
+    for file in selectedFiles:
+        Path = os.path.join(SourceDir, file)
+        Destination = os.path.join(TrainingDir, file)
+        #Move from old path to new dir
+        shutil.move(Path, Destination)
+    print(f"Completed transfer of {len(selectedFiles)} files of healthy brain scans to training data")
+
+def moveGliomaFilesToTraining():
+    #For healthy...
+    SourceDir = "./images/glioma/"
+    TrainingDir = "./TrainingData/glioma/"
+
+    #Error handling (making sure both folders exist)
+    if not os.path.exists(SourceDir):
+        print(SourceDir, "does not exist")
+        return 
+    if not os.path.exists(TrainingDir):
+        print(TrainingDir, "does not exist")
+        return 
+
+    Files = os.listdir(SourceDir)
+    #Calculate 75 percent of the files to be moved
+    numToMove = int(len(Files) * .75)
+    #Parse 75 percent of the files to a new array
+    selectedFiles = Files[:numToMove]
+
+    #loop through all files in selectedFiles
+    for file in selectedFiles:
+        Path = os.path.join(SourceDir, file)
+        Destination = os.path.join(TrainingDir, file)
+        #Move from old path to new dir
+        shutil.move(Path, Destination)
+    print(f"Completed transfer of {len(selectedFiles)} files of glioma brain scans to training data")
+
+def moveMeningiomaFilesToTraining():
+    #For healthy...
+    SourceDir = "./images/meningioma/"
+    TrainingDir = "./TrainingData/meningioma/"
+
+    #Error handling (making sure both folders exist)
+    if not os.path.exists(SourceDir):
+        print(SourceDir, "does not exist")
+        return 
+    if not os.path.exists(TrainingDir):
+        print(TrainingDir, "does not exist")
+        return 
+
+    Files = os.listdir(SourceDir)
+    #Calculate 75 percent of the files to be moved
+    numToMove = int(len(Files) * .75)
+    #Parse 75 percent of the files to a new array
+    selectedFiles = Files[:numToMove]
+
+    #loop through all files in selectedFiles
+    for file in selectedFiles:
+        Path = os.path.join(SourceDir, file)
+        Destination = os.path.join(TrainingDir, file)
+        #Move from old path to new dir
+        shutil.move(Path, Destination)
+    print(f"Completed transfer of {len(selectedFiles)} files of meningioma brain scans to training data")
+
+def movePituitaryFilesToTraining():
+    #For Pituitary...
+    SourceDir = "./images/pituitary/"
+    TrainingDir = "./TrainingData/pituitary/"
+
+    #Error handling (making sure both folders exist)
+    if not os.path.exists(SourceDir):
+        print(SourceDir, "does not exist")
+        return 
+    if not os.path.exists(TrainingDir):
+        print(TrainingDir, "does not exist")
+        return 
+
+    Files = os.listdir(SourceDir)
+    #Calculate 75 percent of the files to be moved
+    numToMove = int(len(Files) * .75)
+    #Parse 75 percent of the files to a new array
+    selectedFiles = Files[:numToMove]
+
+    #loop through all files in selectedFiles
+    for file in selectedFiles:
+        Path = os.path.join(SourceDir, file)
+        Destination = os.path.join(TrainingDir, file)
+        #Move from old path to new dir
+        shutil.move(Path, Destination)
+    print(f"Completed transfer of {len(selectedFiles)} files of pituitary brain scans to training data")
+
+#moveGliomaFilesToTraining()
+
+#moveHealthyFilesToTraining()
+
+#moveMeningiomaFilesToTraining()
+
+#movePituitaryFilesToTraining()
+
+
+
+
+#Needed to interpret output of the model 
+classNames = ['glioma', 'healthy', 'meningioma', 'pituitary']
+
+
+#The shape of each scan (512x512 pixels, rgb 3 colors)
+#print(cv2.imread('TrainingData/glioma/0000.jpg').shape)
+
+#Have colors between 0-1
+train = ImageDataGenerator(rescale = 1/255)
+validation = ImageDataGenerator(rescale = 1/255)
+test = ImageDataGenerator(rescale = 1/255)
+
+#Create dataset that can be fed to NN
+
+trainData = train.flow_from_directory('TrainingData', 
+                                      #keep size the same 
+                                      target_size = (512,512),
+                                      #Need categorical class because our output is more than 2 classes 
+                                      class_mode = 'categorical',
+                                      batch_size = 32)
+validationData = validation.flow_from_directory('ValidationData',
+                                                #keep size the same 
+                                                target_size = (512,512),
+                                                #Need categorical class because our output is more than 2 classes 
+                                                class_mode = 'categorical',
+                                                batch_size = 32)
+TestData = validation.flow_from_directory('TestingData',
+                                                #keep size the same 
+                                                target_size = (512,512),
+                                                #Need categorical class because our output is more than 2 classes 
+                                                class_mode = 'categorical',
+                                                batch_size = 32)
+#Indexes for result intepretation
+#glioma: 0, healthy: 1, meningioma: 2, pituitary: 3
+
+#Initialize model 
+model = tf.keras.models.load_model("model.keras")
+
+
+
+
+
+def predictScan(path, model, classNames):
+
+    #Load the image in the specifified parameters we need (512x512)
+    img = load_img(path, target_size=(512,512))
+
+    
+    #Convert the image to an arr for the model 
+    imgAsArr = img_to_array(img) / 255 #want array between 0 and 1
+    
+    imgAsArr = np.expand_dims(imgAsArr, axis=0)
+    
+    #Create an array of final weights predicted by the model 
+    predictionProbabilities = model.predict(imgAsArr)
+    #Get index of the highest probability 
+    predictionIndex = np.argmax(predictionProbabilities)
+
+    #Get the index of the correct prediction of all types of tumors possible in the model 
+    predictedClass = classNames[predictionIndex]
+    #Take the max of the prediction array (will be confidence %)
+    confidence = np.max(predictionProbabilities)
+    #Return result to user
+    return(f"The model predicts with {confidence * 100:.2f}% confidence that the brain scan shows a {predictedClass} brain.")
+
+def predictUploadedScan(uploadedImg, model, classNames):
+
+    #Load the image in the specifified parameters we need (512x512)
+    img = uploadedImg.resize((512, 512))
+
+    
+    #Convert the image to an arr for the model 
+    imgAsArr = img_to_array(img) / 255 #want array between 0 and 1
+    
+    imgAsArr = np.expand_dims(imgAsArr, axis=0)
+    
+    #Create an array of final weights predicted by the model 
+    predictionProbabilities = model.predict(imgAsArr)
+    #Get index of the highest probability 
+    predictionIndex = np.argmax(predictionProbabilities)
+
+    #Get the index of the correct prediction of all types of tumors possible in the model 
+    predictedClass = classNames[predictionIndex]
+    #Take the max of the prediction array (will be confidence % when multiplied by 100)
+    confidence = np.max(predictionProbabilities)
+
+    if (confidence * 100) < 60:
+        return "The model cannot confidendely make a prediction on this image.", predictionProbabilities
+    #Return result to user
+    if predictedClass == classNames[1]:
+        return (f"The model predicts with {confidence * 100:.2f}% confidence that the brain scan shows no tumor."), predictionProbabilities
+    else:
+        return(f"The model predicts with {confidence * 100:.2f}% confidence that the brain scan shows a {predictedClass} brain tumor."), predictionProbabilities
+
+
+def main():
+   
+    st.markdown('<h1 style="color:#022645; font-size: 40px; text-align: center;"> Tumor Detection by Joey Mosko</h1>', unsafe_allow_html=True)
+    #st.markdown('<h1 style="color:#022645;"> Tumor Detection by Joe Mosko</h1>', unsafe_allow_html=True)
+    st.markdown('<p style="color:#022645; font-size: 30px; text-align: center;"> This machine learning model detects glioma, pituitary, meningioma, and pituitary tumors, or if the brain scan shows no tumor by using thousands of sample images to trian the model on.</p>', unsafe_allow_html=True)
+
+    
+    uploadedFile = st.file_uploader("Start by uploading your image here (jpg or png)", type=["jpg", "jpeg", "png"])
+    if uploadedFile is not None:
+        img = Image.open(uploadedFile)
+        with st.spinner("Analyzing brain scan..."):
+            result, probabilities  = predictUploadedScan(img, model, classNames)
+        if st.button("View uploaded image"):
+            st.image(img)
+        st.write(result)
+
+    if st.button("View graph of probalilities"):
+       fig, ax = plt.subplots()
+       ax.bar(classNames, np.squeeze(probabilities * 100))
+       ax.set_ylabel("Probability %")
+       ax.set_title("Prediction Probabilities")
+       st.pyplot(fig)
+main()
+
+
---
+++ mriModel.py
@@ -0,0 +1,216 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+import cv2 
+import shutil
+# https://www.kaggle.com/datasets/rm1000/brain-tumor-mri-scans/data 
+
+
+'''
+Need to split the data into...
+70% training
+15% test
+15% validation
+Each folder having 4 subfolders for each type of brain scan
+'''
+
+
+def moveHealthyFilesToTraining():
+
+    #For healthy...
+    SourceDir = "./images/healthy/"
+    TrainingDir = "./TrainingData/healthy/"
+
+    #Error handling (making sure both folders exist)
+    if not os.path.exists(SourceDir):
+        print(SourceDir, "does not exist")
+        return 
+    if not os.path.exists(TrainingDir):
+        print(TrainingDir, "does not exist")
+        return 
+
+    Files = os.listdir(SourceDir)
+    #Calculate 75 percent of the files to be moved
+    numToMove = int(len(Files) * .75)
+    #Parse 75 percent of the files to a new array
+    selectedFiles = Files[:numToMove]
+
+    #loop through all files in selectedFiles
+    for file in selectedFiles:
+        Path = os.path.join(SourceDir, file)
+        Destination = os.path.join(TrainingDir, file)
+        #Move from old path to new dir
+        shutil.move(Path, Destination)
+    print(f"Completed transfer of {len(selectedFiles)} files of healthy brain scans to training data")
+
+def moveGliomaFilesToTraining():
+    #For healthy...
+    SourceDir = "./images/glioma/"
+    TrainingDir = "./TrainingData/glioma/"
+
+    #Error handling (making sure both folders exist)
+    if not os.path.exists(SourceDir):
+        print(SourceDir, "does not exist")
+        return 
+    if not os.path.exists(TrainingDir):
+        print(TrainingDir, "does not exist")
+        return 
+
+    Files = os.listdir(SourceDir)
+    #Calculate 75 percent of the files to be moved
+    numToMove = int(len(Files) * .75)
+    #Parse 75 percent of the files to a new array
+    selectedFiles = Files[:numToMove]
+
+    #loop through all files in selectedFiles
+    for file in selectedFiles:
+        Path = os.path.join(SourceDir, file)
+        Destination = os.path.join(TrainingDir, file)
+        #Move from old path to new dir
+        shutil.move(Path, Destination)
+    print(f"Completed transfer of {len(selectedFiles)} files of glioma brain scans to training data")
+
+def moveMeningiomaFilesToTraining():
+    #For healthy...
+    SourceDir = "./images/meningioma/"
+    TrainingDir = "./TrainingData/meningioma/"
+
+    #Error handling (making sure both folders exist)
+    if not os.path.exists(SourceDir):
+        print(SourceDir, "does not exist")
+        return 
+    if not os.path.exists(TrainingDir):
+        print(TrainingDir, "does not exist")
+        return 
+
+    Files = os.listdir(SourceDir)
+    #Calculate 75 percent of the files to be moved
+    numToMove = int(len(Files) * .75)
+    #Parse 75 percent of the files to a new array
+    selectedFiles = Files[:numToMove]
+
+    #loop through all files in selectedFiles
+    for file in selectedFiles:
+        Path = os.path.join(SourceDir, file)
+        Destination = os.path.join(TrainingDir, file)
+        #Move from old path to new dir
+        shutil.move(Path, Destination)
+    print(f"Completed transfer of {len(selectedFiles)} files of meningioma brain scans to training data")
+
+def movePituitaryFilesToTraining():
+    #For Pituitary...
+    SourceDir = "./images/pituitary/"
+    TrainingDir = "./TrainingData/pituitary/"
+
+    #Error handling (making sure both folders exist)
+    if not os.path.exists(SourceDir):
+        print(SourceDir, "does not exist")
+        return 
+    if not os.path.exists(TrainingDir):
+        print(TrainingDir, "does not exist")
+        return 
+
+    Files = os.listdir(SourceDir)
+    #Calculate 75 percent of the files to be moved
+    numToMove = int(len(Files) * .75)
+    #Parse 75 percent of the files to a new array
+    selectedFiles = Files[:numToMove]
+
+    #loop through all files in selectedFiles
+    for file in selectedFiles:
+        Path = os.path.join(SourceDir, file)
+        Destination = os.path.join(TrainingDir, file)
+        #Move from old path to new dir
+        shutil.move(Path, Destination)
+    print(f"Completed transfer of {len(selectedFiles)} files of pituitary brain scans to training data")
+
+#moveGliomaFilesToTraining()
+
+#moveHealthyFilesToTraining()
+
+#moveMeningiomaFilesToTraining()
+
+#movePituitaryFilesToTraining()
+
+
+
+
+#Needed to interpret output of the model 
+classNames = ['glioma', 'healthy', 'meningioma', 'pituitary']
+
+
+#The shape of each scan (512x512 pixels, rgb 3 colors)
+print(cv2.imread('TrainingData/glioma/0000.jpg').shape)
+
+#Have colors between 0-1
+train = ImageDataGenerator(rescale = 1/255)
+validation = ImageDataGenerator(rescale = 1/255)
+test = ImageDataGenerator(rescale = 1/255)
+
+#Create dataset that can be fed to NN
+
+trainData = train.flow_from_directory('TrainingData', 
+                                      #keep size the same 
+                                      target_size = (512,512),
+                                      #Need categorical class because our output is more than 2 classes 
+                                      class_mode = 'categorical',
+                                      batch_size = 32)
+validationData = validation.flow_from_directory('ValidationData',
+                                                #keep size the same 
+                                                target_size = (512,512),
+                                                #Need categorical class because our output is more than 2 classes 
+                                                class_mode = 'categorical',
+                                                batch_size = 32)
+TestData = validation.flow_from_directory('TestingData',
+                                                #keep size the same 
+                                                target_size = (512,512),
+                                                #Need categorical class because our output is more than 2 classes 
+                                                class_mode = 'categorical',
+                                                batch_size = 32)
+#Indexes for result intepretation
+#glioma: 0, healthy: 1, meningioma: 2, pituitary: 3
+tf.ENABLE.ONEDNN.OPTS= 0
+
+#create model 
+
+model = tf.keras.Sequential([
+    #first layer (transforms format of image from a 2d array to a 1d array)
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(512,512,3)),
+    #
+    tf.keras.layers.MaxPool2D(2,2),
+     tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    #
+    tf.keras.layers.MaxPool2D(2,2),
+    #
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPool2D(2,2),
+
+    tf.keras.layers.Flatten(), 
+
+    tf.keras.layers.Dense(512, activation='relu'), 
+    #prevent overfitting - where the model becomes innacurate based on too much data 
+    tf.keras.layers.Dropout(0.5),
+    #4 and softmax activation because we have 4 outcomes 
+    tf.keras.layers.Dense(4, activation='softmax')
+])
+
+#compile model 
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+#fit the model with the training data
+modelFit = model.fit(trainData, epochs=6, validation_data = validationData)
+
+probablityModel  = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+
+testLoss, testAccuracy = model.evaluate(TestData)
+
+print(f"Test Accuracy: {testAccuracy * 100:.2f}%")
+print(f"Test Loss: {testLoss:.4f}")
+
+
+model.save("model.keras")
